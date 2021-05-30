10a10e7e892a9aa2050853457d2d68a8adec8bb3
Dominika2G <dominikagoralczyk5@gmail.com>
2021-05-30 16:12:59 +0200

    Adding posibility of changing user data

---
+++ Models/Authentication/UserDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlannerApi.Models.Authentication
+{
+    public class UserDetails
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string RoleId { get; set; }
+    }
+}
